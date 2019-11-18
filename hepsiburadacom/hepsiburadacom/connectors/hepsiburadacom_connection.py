b101c518552d5b70ad5f2c440173afc3fa65b6fa
tufan@kaynak.gen.tr <!r0nM4!d3n!>
2019-11-19 00:37:52 +0300

    initiation

--- hepsiburadacom/hepsiburadacom/connectors/hepsiburadacom_connection.py
+++ hepsiburadacom/hepsiburadacom/connectors/hepsiburadacom_connection.py
@@ -5,9 +5,11 @@ from requests.auth import HTTPBasicAuth
 
 
 @frappe.whitelist()
-def test_listintegration(username, password, authenticationtype):
+def check_integration(username, password, merchantid):
     servicemethod = 'GET'
-    serviceurl = "https://listing-external-sit.hepsiburada.com"
+    serviceprotocol = "https://"
+    servicehost = "listing-external-sit.hepsiburada.com"
+    serviceurl = serviceprotocol + servicehost
     serviceendpoint = "/listings/merchantid"
 
     servicecontenttype = "application/json"
@@ -15,25 +17,16 @@ def test_listintegration(username, password, authenticationtype):
     merchantid = "509778cf-7104-4c7f-850f-e14fdf5beb70"
     serviceresource = "/" + merchantid
 
+    username = 'framras_dev'
+    password = 'Fr12345!'
+
     servicedata = ""
-
-    req = Request(serviceurl + serviceendpoint + serviceresource, authenticationtype=)
-    req.add_header("Content-Type", servicecontenttype)
-    req.method = servicemethod
-
-    # Create an OpenerDirector with support for Basic HTTP Authentication...
-    auth_handler = HTTPBasicAuthHandler()
-    auth_handler.add_password(user='framras_dev',
-                              passwd='Fr12345!')
-    opener = build_opener(auth_handler)
-    # ...and install it globally so it can be used with urlopen.
-    install_opener(opener)
-
     try:
-        response = urlopen(req)
-        with response as f:
-            return f.read().decode('utf-8')
+        r = requests.get(serviceurl + serviceendpoint + serviceresource, auth=HTTPBasicAuth(username, password))
+        with r:
+            print(r.text)
     except HTTPError as e:
-        return 'The server couldn\'t fulfill the request. ' + 'Error code: ' + str(e.code)
-    except URLError as e:
-        return 'We failed to reach a server. ' + 'Reason: ' + e.reason
+        print('The server couldn\'t fulfill the request. ' + 'Error code: ' + str(e.code))
+    finally:
+        print(r.request.url)
+        print(r.request.headers)
--- hepsiburadacom/hepsiburadacom/sil2.py
+++ hepsiburadacom/hepsiburadacom/sil2.py
@@ -26,25 +26,3 @@ except HTTPError as e:
 finally:
     print(r.request.url)
     print(r.request.headers)
-
-# req = Request()
-# req.add_header("Content-Type", servicecontenttype)
-# req.add_header("Host", servicehost)
-# userAndPass = base64.b64encode(username + ":" + password).decode("utf-8")
-# req.add_header("Authorization", userAndPass)
-# req.method = servicemethod
-
-# Create an OpenerDirector with support for Basic HTTP Authentication...
-# auth_handler = HTTPBasicAuthHandler()
-# auth_handler.add_password(realm=None,
-#                           uri=serviceurl,
-#                           user=username,
-#                           passwd=password)
-# opener = build_opener(auth_handler)
-# # ...and install it globally so it can be used with urlopen.
-# install_opener(opener)
-
-# print(req.headers)
-# print(req.full_url)
-
-#     response = urlopen(req)
